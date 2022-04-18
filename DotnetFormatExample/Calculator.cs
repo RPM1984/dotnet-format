0cc09e0983e1c307ce5aaf1988d13a22c3569bdd
RPM1984 <rpmir1@gmail.com>
2022-04-19 08:55:21 +1000

    Made classes more meaninful

---
+++ DotnetFormatExample/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace DotnetFormatExample
+{
+    public class Calculator
+    {
+        public int AddNumbers(int one, int two)
+        {
+            int finalNumber = 0;
+
+            finalNumber = one + two;
+
+            return finalNumber;
+        }
+    }
+}
